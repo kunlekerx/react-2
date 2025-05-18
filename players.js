3a724726bacfcd59ef1faeab8d1fd341cc99d65a
kunlekerx <habibajibola02@gmail.com>
2025-05-18 22:26:19 +0100

    Create players.js

---
+++ players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Lionel_Messi_20180626.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/35/Kylian_Mbappé_2019.jpg"
+  },
+  {
+    name: "Alex Morgan",
+    team: "San Diego Wave",
+    nationality: "USA",
+    jerseyNumber: 13,
+    age: 34,
+    image: "https://upload.wikimedia.org/wikipedia/commons/7/72/Alex_Morgan_in_2019_%28cropped%29.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 24,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/87/Erling_Haaland_2023.jpg"
+  }
+];
+
+export default players;
